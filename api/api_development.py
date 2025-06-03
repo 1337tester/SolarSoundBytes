aa7367f025fc14bbbf3bf0ae6a65568beb4470c2
Steffen Lauterbach <steffen.lauter@web.de>
2025-06-03 10:14:33 +0200

    simple prediction implemented

--- api/api_development.py
+++ api/api_development.py
@@ -8,4 +8,8 @@ app = FastAPI()
 def index():
     return {'Hola SolarSoundBytes :) '}
 
+# predict endpoint, just for workflow implementation
 
+@app.get('/predict')
+def predict(value_1, value_2):
+    return dict(result = int(value_1)-int(value_2))
