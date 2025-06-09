8ff85e3e46632cde67ef9b404cf9e015fde77caa
Steffen Lauterbach <steffen.lauter@web.de>
2025-06-09 11:46:38 +0200

    adapted to output of sent analyis twitter

--- solarsoundbytes/import_twitter_sent_analysis.py
+++ solarsoundbytes/import_twitter_sent_analysis.py
@@ -1,3 +1,16 @@
-a = 5
-b = 10
-c = a+b  
+import pandas as pd
+import os
+
+
+def create_df_of_twitter_result():
+    base_path = os.path.dirname(__file__)
+    file_path = os.path.join(base_path, 'data_test', 'twitter_sentiment_analysis_UTF8.csv')
+
+    data = pd.read_csv(file_path,  encoding='utf-8')
+
+    df = data[['createdAt', 'sentiment', 'confidence score']]
+    return df
+
+
+test = create_df_of_twitter_result()
+print(test.head())
