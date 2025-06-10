a3cd322f3352448aa5bcae83c9fd6427a43fce7e
Steffen Lauterbach <steffen.lauter@web.de>
2025-06-10 20:02:17 +0200

    updated

--- website/home.py
+++ website/home.py
@@ -1,6 +1,16 @@
 import streamlit as st
 
+# The order in this list determines the order in the sidebar
+# pages = [
+#     st.Page("home.py", title="Home page", icon="🏠"),
+#     st.Page("pages/team.py", title="Team"),
+#     st.Page("pages/app.py", title="Dashboard"),
+# ]
+
+# # Erstellen Sie die Navigation
+# st.navigation(pages)
+
+
 
 st.header('Welcome to SolarSoundBytes')
 st.write('our Dashboard to show ...')
-st.write('the Team: ')
