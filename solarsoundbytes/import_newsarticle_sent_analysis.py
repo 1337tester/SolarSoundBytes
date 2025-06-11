f866f72408b0b40a5b9bba5eee50e86441d5f84b
Steffen Lauterbach <steffen.lauter@web.de>
2025-06-11 20:49:19 +0200

    modified for new datasets

--- solarsoundbytes/import_newsarticle_sent_analysis.py
+++ solarsoundbytes/import_newsarticle_sent_analysis.py
@@ -3,16 +3,18 @@ import os
 
 
 def create_df_of_newsarticle_result():
-    base_path = os.path.dirname(__file__)
-    file_path = os.path.join(base_path, 'data_test', 'cleantech_predictions_with_confidence _UTF8.xlsx')
+    # base_path = os.path.dirname(__file__)
+    # file_path = os.path.join(base_path, 'data_test', 'cleantech_predictions_with_confidence _UTF8.xlsx')
+    csv_path = os.path.normpath(os.path.join(os.path.dirname(__file__), '..', 'data', 'csv', 'combined_articles_with_sentiment.csv'))
 
-    data = pd.read_excel(file_path)
+    data = pd.read_csv(csv_path)
 
-    df = data[['Date Published', 'predicted_sentiment', 'confidence_score']]
-    df = df.rename(columns={'Date Published': 'published',
-                            'predicted_sentiment': 'sentiment',
-                            'confidence_score': 'confidence score'})
+    df = data[['Clean_Date', 'distilbert_pos_score', 'distilbert_neg_score']]
+    df = df.rename(columns={'distilbert_pos_score': 'pos_score',
+                            'Clean_Date': 'date',
+                            'distilbert_neg_score': 'neg_score'})
     return df
 
 
 test = create_df_of_newsarticle_result()
+# print(test.head())
