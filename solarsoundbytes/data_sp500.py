c126d30cc9bafd024c3c7e276342ed7286628c64
Steffen Lauterbach <steffen.lauter@web.de>
2025-06-11 20:48:35 +0200

    modified for new dataset

--- solarsoundbytes/data_sp500.py
+++ solarsoundbytes/data_sp500.py
@@ -3,12 +3,14 @@ import os
 
 def get_sp500_df():
 
-    base_path = os.path.dirname(__file__)
-    file_path = os.path.join(base_path, 'data_test','sp500_2022_2024_dataset.csv')
+    # base_path = os.path.dirname(__file__)
+    # file_path = os.path.join(base_path, 'data_test','sp500_2022_2024_dataset.csv')
     # path_2_data = path_2_root + 'data_test/'
-    # filename = 'sp500_2022_2024_dataset.csv'
+    # filename = 'sp500_2022_2024_dataset.csv
+    csv_path = os.path.normpath(os.path.join(os.path.dirname(__file__), '..', 'data', 'csv', 'sp500_2022_2024_dataset.csv'))
+
     df = pd.read_csv(
-        file_path,
+        csv_path,
         sep=';')
     df_sp500 = df[['Date', 'Price']]
     return df_sp500
