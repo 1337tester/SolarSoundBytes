36469abc74c6c8d61fcf8965dfe94fb7192cf7d6
Steffen Lauterbach <steffen.lauter@web.de>
2025-06-12 10:27:42 +0200

    updated

--- website/pages/app.py
+++ website/pages/app.py
@@ -1,9 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import altair as alt
 import plotly.graph_objects as go
-import plotly.express as px
 
 from streamlit_plotly_events import plotly_events
 
